96639d2ce9b5f66ef3c4de40149dbf3c0860d615
MID3EE-ARCH <realphoenix484@gmail.com>
2024-06-18 20:02:32 +0530

    Solved the program

---
+++ pat5.c
@@ -0,0 +1,22 @@
+// Inverted Left Half Pyramid
+
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the value of n:\n");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int s = 0; s < i; s ++ ){
+            printf(" ");
+        }
+        for (int j = n-i; j > 0; j--)
+        {
+               printf("*") ;
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
